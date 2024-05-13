093cfac1f4d5f82c149a22f4c912b7b50e81a968
RimiBhattacharjee <155084242+RimiBhattacharjee@users.noreply.github.com>
2024-05-13 20:21:29 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "Rimi Bhattacharjee",
+        email: "rimi1995@gmail.com",
+        id: 1
+    },
+    {
+        name: "Manju Bhattacharjee",
+        email: "manju1995@gmail.com",
+        id: 2
+    },
+    {
+        name: "Tapash Bhattacharjee",
+        email: "tapash 1995@gmail.com",
+        id: 3
+    },
+]
